fbf39281dc0add6fa8ae22446726b156a88dd237
xjdrew <xj.drew@gmail.com>
2015-12-28 21:08:38 +0800

    change test name

--- udt/timeout_test.go
+++ udt/timeout_test.go
@@ -6,8 +6,8 @@ import (
 	"time"
 )
 
-func TestWriteTimeout(t *testing.T) {
-	addr := "127.0.0.1:38926"
+func TestReadTimeout(t *testing.T) {
+	addr := getTestAddr()
 	buf := make([]byte, 100)
 	go func() {
 		l, err := Listen("udt", addr)
@@ -15,22 +15,22 @@ func TestWriteTimeout(t *testing.T) {
 			t.Fatal(err)
 		}
 		defer l.Close()
-		c2, err := l.Accept()
-		c2.Read(buf)
+		c1, _ := l.Accept()
+		c1.Read(buf)
 	}()
 
-	c, err := Dial("udt", addr)
+	c0, err := Dial("udt", addr)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer c.Close()
+	defer c0.Close()
 	// timeout 1 second
-	c.SetReadDeadline(time.Now().Add(time.Second))
-	_, err = c.Read(buf)
+	c0.SetReadDeadline(time.Now().Add(time.Second))
+	_, err = c0.Read(buf)
 	if err == nil {
 		t.Fatal("should not be return succeed")
 	}
 	if opError, ok := err.(*net.OpError); !ok || !opError.Timeout() {
-		t.Fatal("should be a timeout error")
+		t.Fatalf("should be a read timeout error:", err)
 	}
 }
